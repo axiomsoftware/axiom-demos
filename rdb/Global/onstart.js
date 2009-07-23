a2bacf2029292a68ac3e55e8b3df6fe3305caac3
Nicholas Campbell <ncampbell@axiomsoftwareinc.com>
2009-07-23 14:49:35 -0400

    insert data instead of erroring on start. :)

--- rdb/Global/onstart.js
+++ rdb/Global/onstart.js
@@ -2,6 +2,7 @@ function init() {
   create_h2_db();
   create_mysql_db();
 
+  addLines('demo', 'H2Page'); //adding in SQL based inserts rather than using the ORM.
 
   for (var i = 0; i < 10; i++) {
     var h2page = new H2Page();
@@ -30,4 +31,16 @@ function create_mysql_db() {
   var conn = getDBConnection('mysql');
   app.log(conn.executeUpdate("CREATE TABLE MySQLPage(ID INT AUTO_INCREMENT PRIMARY KEY, TITLE VARCHAR(255), CONTENT VARCHAR(255));"));
   app.log("last err: "+conn.getLastError());
+}
+
+function addLines(source, table, num) {
+  num = num || 10;
+  app.log("addLines to: "+source+"."+table);
+  var conn = getDBConnection(source);
+  for (var i = 0; i < num; i++) {
+    var sql = "INSERT INTO "+table+" ( TITLE , CONTENT ) VALUES('non hello world h2 -"+i+"', 'BLAM ZOW!');";
+    app.log("Inserting: "+ sql);
+    if (!(conn.executeUpdate(sql)))
+      app.log(conn.getLastError());
+  }
 }
