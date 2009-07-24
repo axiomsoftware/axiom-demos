0fc9dcd51727b0cf4e75ea89c672a05eedf99e1a
Nick <nicholas.j.campbell@gmail.com>
2009-07-24 00:41:32 -0400

    Followers/following...probably still buggy but I'm tired.

--- twax/User/main.js
+++ twax/User/main.js
@@ -1,38 +1,69 @@
 function main() {
+    var is_me = (session && (this === session.user));
     var wrap = this.wrap({});
     default xml namespace = wrap.namespace('');
-    var c = (session && (this == session.user))?this.auth_view():this.unauth_view();
+    var c = (is_me)?this.is_me_view():this.unauth_view();
     wrap.body..div.(@id == 'content')[0].appendChild(c);
-    wrap.body..div.(@id == 'content')[0].appendChild(this.updates({messages: this.get_recent_messages()}));
+    wrap.body..div.(@id == 'content')[0].appendChild(
+	this.updates(
+	    {
+		messages: this.get_recent_messages(is_me)
+	    }
+	)
+    );
+    wrap.body..div.(@id == 'rail')[0].appendChild(this.rail({}));
     return wrap;
 }
 
-function get_recent_messages() {
+function get_recent_messages(user_only) {
     var filters = [
 	{
 	    lastmodifiedby: this.username
 	}
     ];
 
+    if (!user_only && this.followers) {
+	for (var i = 0; i < this.followers.length; i++) {
+	    var follower = this.followers[i].getTarget();
+	    filters.push({lastmodifiedby: follower.username});
+	}
+    }
+
     var results = app.getHits('Message', new OrFilter(filters), {sort:{'_lastmodified': 'desc'}}).objects(0,20);
     return results;
 }
 
-function auth_view() {
+function is_me_view() {
     var view = <>{this.content({})}</>;
     return view;
 }
 
 function unauth_view() {
-    return <>
-	<div>
-	    <ul>
-		<li>{this.username}</li>
-	    </ul>
-	</div>
-	</>;
+    var follow_link = this.getURI();
+    var follow = <a href="">Follow</a>;
+    follow.@href = follow_link+'/follow';
+
+    return follow;
 }
 
 function get_messages() {
 //    var start =
+}
+
+function follow() {
+    if (session.user != this && !(!(this.followers) || this.followers.contains(session.user))) {
+	if (this.followers) {
+	    this.followers = this.followers.concat(new MultiValue(new Reference(session.user)));
+	} else {
+	    this.followers = new MultiValue(new Reference(session.user));
+	}
+
+	if (session.user.following) {
+	    session.user.following = session.user.following.concat(new MultiValue(new Reference(this)));
+	} else {
+	    session.user.following = new MultiValue(new Reference(this));
+	}
+    }
+
+    res.redirect(this.getURI());
 }
